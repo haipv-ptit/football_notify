603b6caf277c1ab5ff1983ffe65a3fe181a6f626
haipv-ptit <haipv.dev@gmail.com>
2018-11-07 09:14:12 +0700

    Apply tag for android notification

--- notification.js
+++ notification.js
@@ -11,34 +11,43 @@ let send = (data) => {
     }
     let time = new Date().getTime()/1000;
     time += 900;
+    let tag = 'match_'+data['id'];
     let message = {
         condition: data['condition'],
         notification: {
             title: data['title'],
-            body: data['body']
+            body: data['body'],
+            tag: tag
         },
         data: data['payload'],
         android: {
-            collapse_key: 'match_'+data['id'],
+            collapse_key: tag,
             ttl: '900s'  // 15 min
         },
         apns: {
             headers:{
                 'apns-expiration': time
             }
-        },
+        }
     };
     //console.log(data['condition']);
     //console.log(data['title'] + '\n' + data['body']+'\n=====');
-    _sendToFcm(message).then(result => {
+    _sendToFcm(message, config.fcm_key).then(result => {
+        //console.log(result);
+    }).catch(err => {
+        //console.error(err);
+    });
+    //
+    let oldKey = 'AAAA0Cv-XUE:APA91bHn81X7L2gZ3HuwRGQbyELk2nx3ROZLWov52rpWQ6T8lWkok5uIgj_I3AGwCCRTuj8JAAhhL002K5taP8iNy8R-z2OkvUi_vlhT6ljCaNac-UkWAvOmGTVEyXun1_zWh5P49Y6V';
+    _sendToFcm(message, oldKey).then(result => {
         //console.log(result);
     }).catch(err => {
         //console.error(err);
     });
 };
 
-let _sendToFcm = async (message) => {
-    let authorization = 'key='+config.fcm_key;
+let _sendToFcm = async (message, key) => {
+    let authorization = 'key='+key;
     return await request.post(fcm_url, {Authorization: authorization}, message);
 };
 
